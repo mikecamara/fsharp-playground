ea2a66f51539cfac8170e67646c1847cafe60f18
Mike Camara <mcmikecamara@gmail.com>
2020-08-01 14:42:27 +0300

    is odd and square functions

---
+++ fsharpConsoleApp/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+//[<EntryPoint>]
+//let main argv =
+    //printfn "%A" argv
+    //0 // return an integer exit code
+
+
+// new code
+module HelloSquare
+
+let square x = x * x
+
+[<EntryPoint>]
+let main argv =
+    printfn "%d squared is: %d!" 12 (square 12)
+    0 // Return an integer exit code
